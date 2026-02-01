3441596bc45d6c80de614e6c2716e25cef195852
abc <abc@abc.abc>
2026-02-01 22:48:35 +0800

    fix(test): include missing QTreeWidget headers in test_gui

--- tests/test_gui.cpp
+++ tests/test_gui.cpp
@@ -2,6 +2,8 @@
 #include <QApplication>
 #include <QPushButton>
 #include <QLineEdit>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
 #include "../src/gui/MainWindow.h"
 
 class TestGui : public QObject {
